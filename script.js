6a32a0747f536b3e9c1c66a7fc1d6eea7ade42fc
SalTheProgrammer <faisal.sudradjat@gmail.com>
2024-07-08 16:27:49 +0800

    fix the display function

--- script.js
+++ script.js
@@ -34,12 +34,12 @@ var elOp = document.getElementsByClassName("operation");
 var eqButton = document.getElementsByClassName("equal");
 var dispBox = document.getElementById("text-display");
 
-var display = function() {
+/*var display = function() {
     var attribute = this.getAttribute("value");
     dispBox.innerHTML = attribute;
     //alert(attribute);
     console.log(attribute);
-};
+};*/
 //add event listeners to numbers and operation buttons
 for (var i = 0; i < elNum.length; i++) {
     elNum[i].addEventListener('click', numPressed, false);
@@ -59,14 +59,16 @@ function numPressed () {
         if (firstNumArray.length < 10) { //check character display length
             firstNumArray.push(this.getAttribute("value"));
             firstNum = firstNumArray.join("");
-            dispBox.innerHTML = firstNum;
+            //dispBox.innerHTML = firstNum;
+            display (firstNum);
         }
     } else { //if operation has been pressed once, it's secondNum
         //charDisp = [];
         if (secondNumArray.length < 10) { //check character display length
             secondNumArray.push(this.getAttribute("value"));
             secondNum = secondNumArray.join("");
-            dispBox.innerHTML = secondNum;
+            //dispBox.innerHTML = secondNum;
+            display (secondNum);
         }
     } 
 }
@@ -77,7 +79,8 @@ function operPressed () {
     firstNum = Number(firstNum);
     secondNum = Number(secondNum);
     if (oper == "AC"){
-        dispBox.innerHTML = 0;
+        //dispBox.innerHTML = 0;
+        display (0);
         firstNumArray = [];
         secondNumArray = [];
         firstNum = 0;
@@ -86,14 +89,16 @@ function operPressed () {
     } else if (opPressCount == 0) { //first time oper is pressed
         oper1 = oper;
         if (secondNumArray.length != 0){
-            dispBox.innerHTML = operate (firstNum, secondNum, oper1);
+            //dispBox.innerHTML = operate (firstNum, secondNum, oper1);
+            display (operate (firstNum, secondNum, oper1));
         }
         opPressCount++;
     } else if (opPressCount > 0 ) { //|| oper == "="
         if (secondNumArray.length != 0) {
             oper2 = oper;
             firstNum = operate (firstNum, secondNum, oper1); 
-            dispBox.innerHTML = firstNum;
+            //dispBox.innerHTML = firstNum;
+            display (firstNum);
             oper1 = oper2;
             secondNumArray = []; //initiate secondNumArray;
         }
@@ -122,12 +127,22 @@ function equalPressed () {
     firstNum = Number(firstNum);
     secondNum = Number(secondNum);
     if (secondNumArray.length != 0) {
-        //if (oper2 == "") {
-            firstNum = operate (firstNum, secondNum, oper); 
-        //} else {
-        //    firstNum = operate (firstNum, secondNum, oper1); 
-        //}
-        dispBox.innerHTML = firstNum;
+        firstNum = operate (firstNum, secondNum, oper); 
+        //dispBox.innerHTML = firstNum;
+        display (firstNum);
         secondNumArray = []; //initiate secondNumArray;
     }
+}
+
+function display (num) {
+    //var len = num.toString().length;
+    n = Number(num);
+    if (n % 1 != 0) {
+        n = n.toFixed(2);
+    }
+    
+    if (n > 10000000) {
+        n = n.toExponential();
+    }
+    dispBox.innerHTML = n;
 }
